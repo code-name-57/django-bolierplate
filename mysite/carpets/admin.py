dc873407dee734e6a98054b37d6316630859bf7c
Avinash Ankur <ankur112358@gmail.com>
2021-06-06 00:19:03 +0530

    :sparkles: simple superuser - admin
    username: admin
    password: admin

--- mysite/carpets/admin.py
+++ mysite/carpets/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Brand, Carpet, Color, Collections, Design, Size
+
+admin.site.register([Brand, Collections, Carpet, Color, Design, Size])
