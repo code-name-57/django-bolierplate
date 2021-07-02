a4013c5d6f1eed6201c30d79d5ae7eeffff63e5d
Vishwajeet Narwal <narwalvishwajeet@gmail.com>
2021-07-02 12:39:52 -0600

    update path of  staticfiles

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -14,13 +14,13 @@ from pathlib import Path
 import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+#BASE_DIR = Path(__file__).resolve().parent.parent
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'mysite/staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
