aab3475c334ebc917c53c8ef879cb89da7c0729c
Vishwajeet Narwal <narwalvishwajeet@gmail.com>
2021-07-07 10:20:54 -0600

    Updated admin interface with tabular inline and AvailableSizes; AvailableColors field

--- carpets/admin.py
+++ carpets/admin.py
@@ -2,6 +2,58 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import *
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
-admin.site.register([Brand, Collections, Color, Design, Size])
-admin.site.register(Carpet, CarpetAdmin)
+class CarpetResource(resources.ModelResource):
+    class Meta:
+        model = Carpet
+
+
+class CarpetAdmin(ImportExportModelAdmin):
+    resource_class = CarpetResource
+    list_display = ['id', 'image_tag', 'design_collection', 'design', 'color', 'size', 'inventory']
+    list_filter = ['design__collection', 'color', 'size']
+    list_editable = ['inventory']
+    search_fields = ['design__name', 'design__collection__name']
+    def design_collection(self, obj):
+        return obj.design.collection.name
+
+class CarpetTabularInline(admin.TabularInline):
+    model = Carpet
+    show_change_link = True
+
+
+class DesignTabularInline(admin.TabularInline):
+    model = Design
+    show_change_link = True
+
+class CollectionTabularInline(admin.TabularInline):
+    model = Collection
+    show_change_link = True
+
+
+class DesignAdmin(admin.ModelAdmin):
+    inlines = [CarpetTabularInline]
+    list_display = ['name', 'collection']
+    list_filter = ['collection', 'collection__brand']
+    filter_horizontal = ('available_colors',)
+    search_fields = ['name', 'collection__name']
+
+class CollectionAdmin(admin.ModelAdmin):
+    inlines = [DesignTabularInline]
+    list_display = ['name', 'brand', 'description']
+    list_filter = ['brand']
+    filter_horizontal = ('available_sizes',)
+    search_fields = ['name']
+
+class BrandAdmin(admin.ModelAdmin):
+    inlines = [CollectionTabularInline]
+    list_display = ['name', 'website']
+    search_fields = ['name']
+
+admin.site.register([Color, Size])
+admin.site.register(Carpet, CarpetAdmin) 
+admin.site.register(Design, DesignAdmin)
+admin.site.register(Collection, CollectionAdmin)
+admin.site.register(Brand, BrandAdmin)
--- carpets/models.py
+++ carpets/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from django.contrib import admin
 from django.db.models.base import Model
 from django.db.models.deletion import CASCADE
-from django.contrib import admin
+
+from django.utils.safestring import mark_safe
+
 
 class Brand(models.Model):
     # Appear as Loloi
@@ -9,32 +12,6 @@ class Brand(models.Model):
     website = models.CharField(max_length=50)
     def __str__(self):
         return self.name
-
-class Collections(models.Model):
-    # Appear as Alvita
-    name = models.CharField(max_length=30)
-    description = models.CharField(max_length=30)
-    brand = models.ForeignKey(Brand, on_delete=CASCADE, null=True)
-    pile_count = models.IntegerField()
-    pile_length = models.DecimalField(decimal_places=2, max_digits=5)
-    def __str__(self):
-        return self.name
-
-class Design(models.Model):
-    # Appear as 6208B
-    name = models.CharField(max_length=30)
-    description = models.CharField(max_length=200)
-    collection = models.ForeignKey(Collections, on_delete=models.CASCADE, null=True)
-    def __str__(self):
-        return self.name
-
-class Color(models.Model):
-    # Appear as Beige/Cream
-    primary_color = models.CharField(max_length=10)
-    texture_color = models.CharField(max_length=10)
-
-    def __str__(self):
-        return (f"{self.primary_color} / {self.texture_color}")
 
 class Size(models.Model):
     SHAPE_CHOICES = (
@@ -53,7 +30,42 @@ class Size(models.Model):
     width = models.DecimalField(decimal_places=2, max_digits=5)
 
     def __str__(self):
-        return (f"{str(self.width)} ft x {str(self.length)} ft")
+        return (f"{str(self.width)} ft x {str(self.length)} ft {self.shape}")
+
+class Color(models.Model):
+    # Appear as Beige/Cream
+    primary_color = models.CharField(max_length=10)
+    texture_color = models.CharField(max_length=10)
+
+    def __str__(self):
+        return (f"{self.primary_color} / {self.texture_color}")
+
+
+class Collection(models.Model):
+    # Appear as Alvita
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=30)
+    brand = models.ForeignKey(Brand, on_delete=CASCADE, null=True)
+    pile_count = models.IntegerField()
+    pile_length = models.DecimalField(decimal_places=2, max_digits=5)
+
+    available_sizes = models.ManyToManyField(Size)
+
+    def __str__(self):
+        return self.name
+
+class Design(models.Model):
+    # Appear as 6208B
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=200)
+    collection = models.ForeignKey(Collection, on_delete=models.CASCADE, null=True)
+
+    available_colors = models.ManyToManyField(Color)
+
+    def __str__(self):
+        return self.name
+
+
 
 class Carpet(models.Model):
     design = models.ForeignKey(Design, on_delete=models.CASCADE, null = True)
@@ -63,17 +75,19 @@ class Carpet(models.Model):
     inventory = models.IntegerField(default=0)
 
     def __str__(self):
-        return (f"carpet_image: {str(self.image_file)} - "
-                f"{self.color} color of "
-                f"in the {self.design} "
-                f"with size {self.size}.")
+        return (f"({str(self.design.collection)}) "
+                f"{self.design} "
+                f"{self.color} -"
+                f"({self.size})")
 
     def in_stock(self):
         return self.inventory > 0
 
-class CarpetAdmin(admin.ModelAdmin):
-    list_display = ['id', 'design_collection', 'design', 'color', 'size', 'inventory']
-    list_editable = ['inventory']
-    search_fields = ['design__name', 'design__collection__name', 'color__primary_color', 'color__texture_color']
-    def design_collection(self, obj):
-        return obj.design.collection.name
+    def image_tag(self):
+        if self.image_file:
+            return mark_safe('<img src="%s" style="width: 45px; height:45px;" />' % self.image_file.url)
+        else:
+            return 'No Image Found'
+    image_tag.short_description = 'Image'
+
+
