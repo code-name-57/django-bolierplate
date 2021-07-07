60e3f593f4717041b5885936ab7d472c9dfa46d1
Vishwajeet Narwal <narwalvishwajeet@gmail.com>
2021-07-07 11:30:16 -0600

    fixing settings for production

--- mysite/settings/prod.py
+++ mysite/settings/prod.py
@@ -5,6 +5,10 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ['SECRET_KEY']
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
@@ -17,9 +21,6 @@ STATICFILES_DIRS = (
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
 
 ALLOWED_HOSTS = ['rugviz.herokuapp.com']
 
