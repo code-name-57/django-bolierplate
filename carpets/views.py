0959d3ced9aa2fdedab9a53e1053efe7e1a55ee7
Avinash Ankur <ankur112358@gmail.com>
2021-07-14 10:58:38 +0530

    :sparkles: basic filter
    filters carpet based upon design and collection, using multiple choice checkboxes
    
    Ref #3

--- carpets/views.py
+++ carpets/views.py
@@ -1,14 +1,25 @@
+from django.db.models import query
+from django.forms import widgets, CheckboxSelectMultiple
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models import Carpet
+import django_filters
+from .models import Carpet, Design, Collection
 from .forms import CarpetImageForm, BrandForm
+
+class CarpetPropertyFilter(django_filters.FilterSet):
+    design = django_filters.ModelMultipleChoiceFilter(queryset=Design.objects.all(), widget=CheckboxSelectMultiple)
+    design__collection = django_filters.ModelMultipleChoiceFilter(queryset=Collection.objects.all(), widget=CheckboxSelectMultiple)
+    class Meta:
+        model = Carpet
+        fields = {'design', 'design__collection'}
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
 def showAllCarpets(request):
     available_carpets = Carpet.objects.all()
-    context = {"carpets": available_carpets}
+    carpet_filter = CarpetPropertyFilter(request.GET, queryset=available_carpets)
+    context = {"filter": carpet_filter}
     return render(request, "carpets_index.html", context)
 
 
