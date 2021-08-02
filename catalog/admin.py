bdacf516f73997aa22b65ff43eec8501afd75e7d
Vishwajeet <vishwajeet.narwal@colorado.edu>
2021-08-02 18:24:08 -0500

    Seperate Catalog app Seperated catalog app for rugs added Design-In-Color through field to store image Ref #16

---
+++ catalog/admin.py
@@ -0,0 +1,88 @@
+from carpets.models import available_color_changed
+from django.contrib import admin
+
+# Register your models here.
+from .models import *
+from import_export import resources
+from import_export.fields import Field
+from import_export.admin import ImportExportModelAdmin
+
+from django.utils.safestring import mark_safe
+
+from django.contrib.admin.widgets import AdminFileWidget
+
+class AdminImageWidget(AdminFileWidget):
+
+    def render(self, name, value, attrs=None, renderer=None):
+        output = []
+
+        if value and getattr(value, "url", None):
+            image_url = value.url
+            file_name = str(value)
+
+            output.append(
+                f' <a href="{image_url}" target="_blank">'
+                f'  <img src="{image_url}" alt="{file_name}" width="150" height="150" '
+                f'style="object-fit: cover;"/> </a>')
+
+        output.append(super(AdminFileWidget, self).render(name, value, attrs, renderer))
+        return mark_safe(u''.join(output))
+
+class CarpetAdmin(admin.ModelAdmin):
+    list_display = ['id', 'image_tag', 'design_collection', 'designColor', 'size', 'inventory']
+    list_filter = ['designColor__design__collection', 'designColor__color', 'size']
+    list_editable = ['inventory']
+    search_fields = ['designColor__design__name', 'designColor__design__collection__name']
+    def design_collection(self, obj):
+        return obj.designColor.design.collection.name
+
+
+class CarpetTabularInline(admin.TabularInline):
+    model = Carpet
+    show_change_link = True
+
+
+class DesignTabularInline(admin.TabularInline):
+    model = Design
+    show_change_link = True
+
+
+
+
+class DesignInColorTabularInline(admin.TabularInline):
+    model = DesignInColor
+    formfield_overrides = {models.ImageField: {'widget': AdminImageWidget}}
+
+    show_change_link = True
+
+
+class CollectionTabularInline(admin.TabularInline):
+    model = Collection
+    show_change_link = True
+
+
+class DesignAdmin(admin.ModelAdmin):
+    inlines = [DesignInColorTabularInline]
+    list_display = ['name', 'collection']
+    list_filter = ['collection', 'collection__brand']
+    filter_horizontal = ('available_colors',)
+    search_fields = ['name', 'collection__name']
+
+class CollectionAdmin(admin.ModelAdmin):
+    inlines = [DesignTabularInline]
+    list_display = ['name', 'brand', 'description']
+    list_filter = ['brand']
+    filter_horizontal = ('available_sizes',)
+    search_fields = ['name']
+
+class BrandAdmin(admin.ModelAdmin):
+    inlines = [CollectionTabularInline]
+    list_display = ['name', 'website']
+    search_fields = ['name']
+
+
+admin.site.register([Color, Size])
+admin.site.register(Carpet, CarpetAdmin) 
+admin.site.register(Design, DesignAdmin)
+admin.site.register(Collection, CollectionAdmin)
+admin.site.register(Brand, BrandAdmin)
---
+++ catalog/models.py
@@ -0,0 +1,111 @@
+from enum import unique
+from django.db import models
+from django.contrib import admin
+from django.db.models import constraints
+from django.db.models.base import Model
+from django.db.models.deletion import CASCADE
+from django.db.models.fields import CharField
+
+from django.utils.safestring import mark_safe
+
+
+class Brand(models.Model):
+    # Appear as Loloi
+    name = models.CharField(max_length=30, unique=True)
+    website = models.CharField(max_length=50, blank=True)
+    def __str__(self):
+        return self.name
+
+class Size(models.Model):
+    SHAPE_CHOICES = (
+    ('Rectangle', 'Rectangle'),
+    ('Oval', 'Oval'),
+    ('Round', 'Round'),
+    )
+    # Constants in Model class
+    RECTANGLE = 'Rectangle'
+    shape = models.CharField(max_length=20,
+                choices=SHAPE_CHOICES,
+                default=RECTANGLE)
+    length = models.DecimalField(decimal_places=2, max_digits=5)
+    width = models.DecimalField(decimal_places=2, max_digits=5)
+
+    class Meta:
+        constraints = [models.UniqueConstraint(fields=['length', 'width', 'shape'], name="unique_size_t")]
+
+    def __str__(self):
+        return (f"{str(self.width)} ft x {str(self.length)} ft {self.shape}")
+
+class Color(models.Model):
+    # Appear as Beige/Cream
+    primary_color = models.CharField(max_length=10)
+    texture_color = models.CharField(max_length=10, blank=True)
+
+    class Meta:
+        constraints = [models.UniqueConstraint(fields=['primary_color', 'texture_color'], name="unique_color_t")]
+
+    def __str__(self):
+        return (f"{self.primary_color} / {self.texture_color}")
+
+
+class Collection(models.Model):
+    # Appear as Alvita
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=200, blank=True)
+    brand = models.ForeignKey(Brand, on_delete=CASCADE, null=True)
+    pile_count = models.IntegerField(blank=True)
+    pile_length = models.DecimalField(decimal_places=2, max_digits=5, blank=True)
+
+    available_sizes = models.ManyToManyField(Size)
+
+    def __str__(self):
+        return self.name
+
+class Design(models.Model):
+    # Appear as 6208B
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=200)
+    collection = models.ForeignKey(Collection, on_delete=models.CASCADE, null=True)
+
+    available_colors = models.ManyToManyField(Color, through='DesignInColor')
+
+    def __str__(self):
+        return self.name
+
+class DesignInColor(models.Model):
+    design = models.ForeignKey(Design, on_delete=models.CASCADE, null = True)
+    color = models.ForeignKey(Color, on_delete=CASCADE, null = True)
+    image_file = models.ImageField(upload_to='catalog/DesignColor', null=True)
+
+    def __str__(self):
+        return (f"({str(self.design.collection)}) "
+                f"{self.design} in "
+                f"{self.color}")
+    def image_tag(self):
+        if self.image_file:
+            return mark_safe('<img src="%s" style="width: 45px; height:45px;" />' % self.image_file.url)
+        else:
+            return 'No Image Found'
+    image_tag.short_description = 'Image'
+
+class Carpet(models.Model):
+    designColor = models.ForeignKey(DesignInColor, on_delete=models.CASCADE, null = True)
+    size = models.ForeignKey(Size, on_delete=models.CASCADE, null = True)
+    image_file = models.ImageField(upload_to='catalog/DesignColorSize', null=True)
+    inventory = models.IntegerField(default=0)
+
+    def __str__(self):
+        return (f"({str(self.designColor)} - {self.size}) ")
+
+    def in_stock(self):
+        return self.inventory > 0
+
+    def image_tag(self):
+        if self.image_file:
+            return mark_safe('<img src="%s" style="width: 45px; height:45px;" />' % self.image_file.url)
+        else:
+            return 'No Image Found'
+    image_tag.short_description = 'Image'
+
+    class Meta:
+        constraints = [models.UniqueConstraint(fields=['designColor','size'], name="unique_ein_t")]
